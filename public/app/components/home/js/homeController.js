2daebba9e694d53b0cff3cf4e4ef6f235dccf172
Sukhesh Gopaluni <sgopaluni@SFSGOPALUNI-MBP.local>
2018-08-22 16:51:28 -0700

    added css changes and home page

--- public/app/components/home/js/homeController.js
+++ public/app/components/home/js/homeController.js
@@ -2,15 +2,63 @@ define(["app",
 	"services/utilService",
 	"directives/alertDirectives"
 ], function (app) {
-	app.controller("homeController", ["$scope", "$translate", "$rootScope", "utilService", "$timeout", function ($scope, $translate, $rootScope, utilService, $timeout) {
+	app.controller("homeController", ["$scope", "$translate", "$rootScope", "utilService", "$timeout", "$http", function ($scope, $translate, $rootScope, utilService, $timeout, $http) {
 		$scope.showAlert = false;
-		$scope.page =
-            {
-            	heading: "Welcome"
-            };
+		$scope.page = {
+			heading: "Welcome"
+		};
 
 		$timeout(function () {
 			$scope.showAlert = true;
 		}, 3000);
+
+		$http.get('http://ipinfo.io/json').
+		then(function (data) {
+			console.log(data);
+			$scope.location = data;
+			$scope.country = 'fr';
+			if (data.country != 'FR') $scope.country = "en";
+		});
+
+		$scope.adList = [
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			},
+			{
+				face : "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+			}
+
+		]
 	}]);
 });
